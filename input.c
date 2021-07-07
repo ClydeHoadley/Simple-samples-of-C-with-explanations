67eac71c9782aa494a43b48cb84b5bda808f5b42
Clyde <47218495+ClydeHoadley@users.noreply.github.com>
2021-07-07 12:52:54 -0600

    What do I put here?
    This is an extended description of what it's all about.

---
+++ input.c
@@ -0,0 +1,24 @@
+ /* Demonstrates using scanf() to input numeric and text data. */
+
+ #include <stdio.h>
+
+ char lname[257], fname[257];
+ int count, id_num;
+
+ int main( void )
+ {
+     /* Prompt the user. */
+
+     puts("Enter last name, first name, ID number separated");
+     puts("by spaces, then press Enter.");
+
+     /* Input the three data items. */
+
+     count = scanf("%s%s%d", lname, fname, &id_num);
+
+     /* Display the data. */
+
+     printf("%d items entered: %s %s %d \n", count, fname, lname, id_num);
+
+     return 0;
+ }
